438bc3c4d11cd86cf2e081f06463d93905c70743
Swapnil Iqbal <swapniliqbal@gmail.com>
2020-01-03 07:50:12 -0500

    Implemented Firebase user signup. New user can now register using email and password. Existing users can't re-register (will throw error). All newly registered user data gets added into my Firebase project's Authentication->Users tab.
    Signed-off-by: Swapnil Iqbal <swapniliqbal@gmail.com>

--- app/src/main/java/com/swapnil/firebaseapp/Register.java
+++ app/src/main/java/com/swapnil/firebaseapp/Register.java
@@ -1,14 +1,60 @@
 package com.swapnil.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    private Button mRegister;
+    private EditText registerEmailInput;
+    private EditText registerPasswordInput;
+
+    //Instance of FirebaseAuth
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mRegister = findViewById(R.id.register);
+        registerEmailInput = findViewById(R.id.registerUsername);
+        registerPasswordInput = findViewById(R.id.registerPassword);
+
+        //Initialize Firebase authentication system
+        mAuth = FirebaseAuth.getInstance();
+
+        //When user clicks register, fetch the data from the editTexts, input into Firebase auth, and throw message.
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.createUserWithEmailAndPassword(
+                        registerEmailInput.getText().toString(),
+                        registerPasswordInput.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(Register.this, task.getException().getMessage(), Toast.LENGTH_LONG).show(); //ie user exists, wrong data, etc
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
