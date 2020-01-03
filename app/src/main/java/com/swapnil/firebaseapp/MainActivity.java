cd464976ab83ad63a8f95a7f9ac0ce10f90cdae1
Swapnil Iqbal <swapniliqbal@gmail.com>
2020-01-03 05:41:15 -0500

    TODO comments added. Will be updated.
    Signed-off-by: Swapnil Iqbal <swapniliqbal@gmail.com>

--- app/src/main/java/com/swapnil/firebaseapp/MainActivity.java
+++ app/src/main/java/com/swapnil/firebaseapp/MainActivity.java
@@ -1,3 +1,6 @@
+// TODO: 01/03/2020 Design activity_main.xml as the login/register page. 
+// TODO: 01/03/2020 import Google's firebase API code into the app and project level build.gradle
+
 package com.swapnil.firebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
