afb232af99d905d25c5a85f40d0e47456d815f85
Swapnil Iqbal <swapniliqbal@gmail.com>
2020-01-03 06:36:01 -0500

    Added login & register pages, implemented button from home page to lead to those pages.
    Signed-off-by: Swapnil Iqbal <swapniliqbal@gmail.com>

--- app/src/main/java/com/swapnil/firebaseapp/MainActivity.java
+++ app/src/main/java/com/swapnil/firebaseapp/MainActivity.java
@@ -1,17 +1,42 @@
-// TODO: 01/03/2020 Design activity_main.xml as the login/register page. 
-// TODO: 01/03/2020 import Google's firebase API code into the app and project level build.gradle
+// TODO: 01/03/2020 Design activity_main.xml as the login/register page.
+// TODO: 01/03/2020 add "Register" title to activity_register.xml
 
 package com.swapnil.firebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mRegister;
+    private Button mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegister = findViewById(R.id.register);
+        mLogin = findViewById(R.id.logIn);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
